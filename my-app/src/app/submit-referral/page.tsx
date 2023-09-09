f85dedd9dcda5af5c83860eadab209eea639347f
kchang4 <2335501+kchang4@users.noreply.github.com>
2023-09-09 04:42:59 +0000

    fix export on submit page

--- my-app/src/app/submit-referral/page.tsx
+++ my-app/src/app/submit-referral/page.tsx
@@ -1,14 +1,10 @@
-import Header from "@/components/Header/Header";
 import { ReferralLinkSubmitForm } from "@/components/ReferralLinkSubmitForm/RerferralLinkSubmitForm";
 import { Box } from "@mui/material";
-import { NextPage } from "next";
 
-const SubmitReferralPage: NextPage = () => {
+export default async function SubmitReferralPage() {
   return (
     <Box>
       <ReferralLinkSubmitForm />
     </Box>
   );
-};
-
-export default SubmitReferralPage;
+}
